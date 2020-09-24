abe855d4997c95c9f7af5fde2628601d64e763d5
Tyler Degand <tylerdegand@gmail.com>
2020-09-24 16:02:25 -0500

    added some validation in

--- models/recipe.js
+++ models/recipe.js
@@ -2,14 +2,24 @@
 
 module.exports = (sequelize, DataTypes) => {
   const Recipe = sequelize.define('recipe', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement:true,
+      unique: true,
+      allowNull:false 
+    },
     name: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      allowNull:false 
     },
     description: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      allowNull:false 
     },
     ingredient: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      allowNull:false 
     }
   });
 
